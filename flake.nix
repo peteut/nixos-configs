66aeb1f3546d292a214ea62fca398d73f1aee6ec
Alain Péteut <alain.peteut@yahoo.com>
2022-10-08 10:02:10 +0200

    flake: update deps.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My deploy-rs config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     deploy-rs = {
       url = "github:serokell/deploy-rs";
