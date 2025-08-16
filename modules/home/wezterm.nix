ff18ec6d5317ba82ec418867da27b00d28779067
Alain Péteut <alain.peteut@spacetek.ch>
2025-08-16 23:31:11 +0200

    wezterm: disable default keybindings.

--- modules/home/wezterm.nix
+++ modules/home/wezterm.nix
@@ -5,12 +5,11 @@ let
     path = "wez-pain-control";
     source = fetchFromGitHub {
       owner = "sei40kr";
-      rev = "main";
       repo = "wez-pain-control";
+      rev = "main";
       sha256 = "sha256-GT4oeCF/FZJCRTiSzTV1Wt3EJ26Z90iUO/OqxmB1Ods=";
     };
   };
-
 in
 {
   programs.wezterm = {
@@ -20,7 +19,9 @@ in
 
       config.hide_tab_bar_if_only_one_tab = true
       config.mux_enable_ssh_agent = false
+      config.disable_default_key_bindings = true
       config.keys = {
+        { key = 'L', mods = 'CTRL', action = wezterm.action.ShowDebugOverlay },
       }
       config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 }
       require("${wezPainControl.path}.plugin").apply_to_config(config, {})
