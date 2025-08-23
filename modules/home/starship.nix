4e134808d4ef56e47a260365b43d28e4d9874a39
Alain Péteut <alain.peteut@spacetek.ch>
2025-08-23 23:12:45 +0200

    starship: add hostname, adjust time format.

--- modules/home/starship.nix
+++ modules/home/starship.nix
@@ -5,7 +5,14 @@
     settings = {
       add_newline = false;
       shell.disabled = false;
-      time.disabled = false;
+      time = {
+        disabled = false;
+        time_format = "%c";
+      };
+      hostname = {
+        disabled = false;
+        ssh_only = false;
+      };
     };
   };
 }
