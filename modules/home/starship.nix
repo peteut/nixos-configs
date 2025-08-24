bd66491ee1e2e0c73683600e519469bea4f7ab4c
Alain Péteut <alain.peteut@spacetek.ch>
2025-08-24 20:52:40 +0200

    starship: add battery status.

--- modules/home/starship.nix
+++ modules/home/starship.nix
@@ -13,6 +13,30 @@
         disabled = false;
         ssh_only = false;
       };
+      battery = {
+        disabled = false;
+        full_symbol = "  ";
+        charging_symbol = " ";
+        discharging_symbol = "  ";
+        unknown_symbol = "  ";
+        empty_symbol = "  ";
+        display = [
+          {
+            threshold = 10;
+            style = "bold red";
+            discharging_symbol = "  ";
+          }
+          {
+            threshold = 20;
+            style = "bold yellow";
+            discharging_symbol = "  ";
+          }
+          {
+            threshold = 60;
+            style = "bold green";
+          }
+        ];
+      };
     };
   };
 }
